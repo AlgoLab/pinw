5527f7f15a826ca99f0fb3d9da01286569762ed2
Andrea <andrea.crispo.cr@gmail.com>
2016-03-15 21:21:20 +0100

    Added import/export settings option

--- routes/admin/settings.rb
+++ routes/admin/settings.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require "sinatra/json"
+require "yaml"
 
 class PinW < Sinatra::Application
     get '/admin/settings/?' do
@@ -15,6 +16,44 @@ class PinW < Sinatra::Application
     		setting.value = params[setting.key.to_sym]
     		setting.save
     	end
-    	redirect back
+      redirect to '/admin/settings?ok=1'
     end
+
+    post '/admin/settings/import' do
+        if params[:ImportInput]
+            File.open("#{PROJECT_DATA_PATH}/pinw_settings.yml", "w") do |f|
+                f.write(params[:ImportInput][:tempfile].read)
+            end
+        end
+        if File.exist?("#{PROJECT_DATA_PATH}/pinw_settings.yml")
+            settings_file = YAML.load_file("#{PROJECT_DATA_PATH}/pinw_settings.yml")
+        else
+          redirect to '/admin/settings?err=2'
+        end
+        Settings.all.each do |setting|
+          next if setting.key=="SSH_PRIVATE_KEY"
+          next if setting.key=="SSH_PUBLIC_KEY"
+      		setting.value = settings_file["settings"][setting.key]
+      		setting.save
+      	end
+      	redirect to '/admin/settings?ok=2'
+    end
+
+    post '/admin/settings/export' do
+        File.open("#{PROJECT_DATA_PATH}/pinw_settings.yml","w") do |file|
+          file.write("settings:\n")
+          Settings.all.each do |setting|
+              next if setting.key=="SSH_PRIVATE_KEY"
+              next if setting.key=="SSH_PUBLIC_KEY"
+        		  file.write("  #{setting.key}: #{setting.value}\n")
+        	end
+        end
+        redirect to '/admin/settings?err=3'  unless  File.file? "#{PROJECT_DATA_PATH}/pinw_settings.yml"
+
+        send_file "#{PROJECT_DATA_PATH}/pinw_settings.yml", disposition: 'attachment'
+    end
+
+
+
+
 end
