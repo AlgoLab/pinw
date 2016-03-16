e374dcc83529a4366f6fb9875207bbc8f5356c44
Andrea <andrea.crispo.cr@gmail.com>
2016-03-16 21:33:56 +0100

    Import/Export also ssh keys

--- routes/admin/settings.rb
+++ routes/admin/settings.rb
@@ -31,22 +31,19 @@ class PinW < Sinatra::Application
           redirect to '/admin/settings?err=2'
         end
         Settings.all.each do |setting|
-          next if setting.key=="SSH_PRIVATE_KEY"
-          next if setting.key=="SSH_PUBLIC_KEY"
-      		setting.value = settings_file["settings"][setting.key]
+      		setting.value = settings_file[setting.key]
       		setting.save
       	end
       	redirect to '/admin/settings?ok=2'
     end
 
     post '/admin/settings/export' do
+        s = {}
+        Settings.all.each do |setting|
+            s[setting.key] = setting.value
+        end
         File.open("#{PROJECT_DATA_PATH}/pinw_settings.yml","w") do |file|
-          file.write("settings:\n")
-          Settings.all.each do |setting|
-              next if setting.key=="SSH_PRIVATE_KEY"
-              next if setting.key=="SSH_PUBLIC_KEY"
-        		  file.write("  #{setting.key}: #{setting.value}\n")
-        	end
+          file.write s.to_yaml
         end
         redirect to '/admin/settings?err=3'  unless  File.file? "#{PROJECT_DATA_PATH}/pinw_settings.yml"
 
